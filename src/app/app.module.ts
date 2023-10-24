0a2b9c9d3957fc1f9010a67cd5c268af33c97829
Sergey Sherstobitov <in43sh@gmail.com>
2023-10-23 21:46:31 -0400

    Move everything from search-item to search-results.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     SearchComponent,
     SearchItemComponent,
+    SearchResultsComponent,
   ],
   imports: [
     BrowserModule,
