377be28c1211bb41a7aa8eb6209e0be0eb269687
Sergey Sherstobitov <in43sh@gmail.com>
2023-10-23 20:04:25 -0400

    Create search-item component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { SearchItemComponent } from './search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    SearchItemComponent
   ],
   imports: [
     BrowserModule
